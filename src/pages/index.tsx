b149bc4c7ebcebd0f00aa95000844531010522d0
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-05-04 01:09:59 -0300

    feat: gerando build com SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
-      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    // 60s * 60m * 8h = 24h
+    revalidate: 60 * 60 * 8,
+  };
 }
